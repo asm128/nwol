b38fc7dc4789aca61db6c79c649d39463af20b92
asm128 <pabloarielz@outlook.com>
2017-04-23 04:28:38 -0300

    * Minor formatting fix.

--- gods/reference_registry.cpp
+++ gods/reference_registry.cpp
@@ -9,8 +9,8 @@ uint32_t				nwol::CReferenceContainer::Save					( char* out_pMemoryBuffer )			co
 	::nwol::suint32										dataCount				= DataNames.size();
 
 	totalBytes										+= dataCount.write(out_pMemoryBuffer);
-	for(uint32_t i=0; i<dataCount; i++)	totalBytes		+= DataNames[i].save(out_pMemoryBuffer ? &out_pMemoryBuffer[totalBytes] : 0);
-	for(uint32_t i=0; i<dataCount; i++)	totalBytes		+= DataDescriptions[i].save(out_pMemoryBuffer ? &out_pMemoryBuffer[totalBytes] : 0);
+	for(uint32_t i=0; i<dataCount; i++)	totalBytes		+= DataNames		[i].save(out_pMemoryBuffer ? &out_pMemoryBuffer[totalBytes] : 0);
+	for(uint32_t i=0; i<dataCount; i++)	totalBytes		+= DataDescriptions	[i].save(out_pMemoryBuffer ? &out_pMemoryBuffer[totalBytes] : 0);
 
 	return totalBytes;
 }
