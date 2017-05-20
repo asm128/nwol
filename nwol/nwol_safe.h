f2284f20110f6b963f466c18d769db43a99111e2
asm128 <pabloarielz@outlook.com>
2017-05-19 23:59:38 -0300

    * Fixed safe_closesocket().

--- nwol/nwol_safe.h
+++ nwol/nwol_safe.h
@@ -1,10 +1,11 @@
 #ifndef NWOL_SAFE_H_027409278349234
 #define NWOL_SAFE_H_027409278349234
 
-#define safe_free(p)						if(p) { free				(p); p=0; }
-#define safe_nwol_free(p)					if(p) { ::nwol::nwol_free	(p); p=0; }
-#define safe_delete(p)						if(p) { delete				(p); p=0; }
-#define safe_closesocket(p)					if(p) { closesocket			(p); p=0; }
-#define safe_fclose(p)						if(p) { fclose				(p); p=0; }
+#define safe_free(p)						if(p)						{ free				(p); p = 0; }
+#define safe_delete(p)						if(p)						{ delete			(p); p = 0; }
+#define safe_fclose(p)						if(p)						{ fclose			(p); p = 0; }
+#define safe_closesocket(p)					if(p != INVALID_SOCKET)		{ closesocket		(p); p = INVALID_SOCKET; }
+
+#define safe_nwol_free(p)					if(p)						{ ::nwol::nwol_free	(p); p = 0; }
 
 #endif // NWOL_SAFE_H_027409278349234
