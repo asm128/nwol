e38d737d0b7b154dbbe90f49776670b1c4994660
asm128 <pabloarielz@outlook.com>
2017-07-02 06:30:27 -0300

    + Added isPowerOfTwo().

--- nwol/nwol_coord.h
+++ nwol/nwol_coord.h
@@ -13,35 +13,35 @@ namespace nwol
 		typedef			SCoord2<_tBase>	TCoord2;
 							_tBase		x, y;
 		//
-		inline constexpr	TCoord2		operator+				(const TCoord2& other)										const	noexcept	{ return {x + other.x, y + other.y};																					}
-		inline constexpr	TCoord2		operator-				(const TCoord2& other)										const	noexcept	{ return {x - other.x, y - other.y};																					}
-		inline constexpr	TCoord2		operator*				(double scalar)												const	noexcept	{ return {(_tBase)(x * scalar), (_tBase)(y * scalar)};																	}
-		inline constexpr	TCoord2		operator/				(double scalar)												const				{ return {(_tBase)(x / scalar), (_tBase)(y / scalar)};																	}
-		inline constexpr	TCoord2		operator*				(int64_t scalar)											const	noexcept	{ return {(_tBase)(x * scalar), (_tBase)(y * scalar)};																	}
-		inline constexpr	TCoord2		operator/				(int64_t scalar)											const				{ return {(_tBase)(x / scalar), (_tBase)(y / scalar)};																	}
-		inline constexpr	TCoord2		operator*				(uint64_t scalar)											const	noexcept	{ return {(_tBase)(x * scalar), (_tBase)(y * scalar)};																	}
-		inline constexpr	TCoord2		operator/				(uint64_t scalar)											const				{ return {(_tBase)(x / scalar), (_tBase)(y / scalar)};																	}
+		inline constexpr	TCoord2		operator+				(const TCoord2& other)										const	noexcept	{ return {x + other.x, y + other.y};																							}
+		inline constexpr	TCoord2		operator-				(const TCoord2& other)										const	noexcept	{ return {x - other.x, y - other.y};																							}
+		inline constexpr	TCoord2		operator*				(double scalar)												const	noexcept	{ return {(_tBase)(x * scalar), (_tBase)(y * scalar)};																			}
+		inline constexpr	TCoord2		operator/				(double scalar)												const				{ return {(_tBase)(x / scalar), (_tBase)(y / scalar)};																			}
+		inline constexpr	TCoord2		operator*				(int64_t scalar)											const	noexcept	{ return {(_tBase)(x * scalar), (_tBase)(y * scalar)};																			}
+		inline constexpr	TCoord2		operator/				(int64_t scalar)											const				{ return {(_tBase)(x / scalar), (_tBase)(y / scalar)};																			}
+		inline constexpr	TCoord2		operator*				(uint64_t scalar)											const	noexcept	{ return {(_tBase)(x * scalar), (_tBase)(y * scalar)};																			}
+		inline constexpr	TCoord2		operator/				(uint64_t scalar)											const				{ return {(_tBase)(x / scalar), (_tBase)(y / scalar)};																			}
 		//
-		inline				TCoord2&	operator+=				(const TCoord2& other)												noexcept	{ x += other.x; y += other.y;							return *this;													}
-		inline				TCoord2&	operator-=				(const TCoord2& other)												noexcept	{ x -= other.x; y -= other.y;							return *this;													}
-		inline				TCoord2&	operator*=				(double scalar)														noexcept	{ x = (_tBase)(x * scalar); y = (_tBase)(y * scalar);	return *this;													}
-		inline				TCoord2&	operator/=				(double scalar)																	{ x = (_tBase)(x / scalar); y = (_tBase)(y / scalar);	return *this;													}
-		inline				TCoord2&	operator*=				(int64_t scalar)													noexcept	{ x = (_tBase)(x * scalar); y = (_tBase)(y * scalar);	return *this;													}
-		inline				TCoord2&	operator/=				(int64_t scalar)																{ x = (_tBase)(x / scalar); y = (_tBase)(y / scalar);	return *this;													}
-		inline				TCoord2&	operator*=				(uint64_t scalar)													noexcept	{ x = (_tBase)(x * scalar); y = (_tBase)(y * scalar);	return *this;													}
-		inline				TCoord2&	operator/=				(uint64_t scalar)																{ x = (_tBase)(x / scalar); y = (_tBase)(y / scalar);	return *this;													}
+		inline				TCoord2&	operator+=				(const TCoord2& other)												noexcept	{ x += other.x; y += other.y;							return *this;															}
+		inline				TCoord2&	operator-=				(const TCoord2& other)												noexcept	{ x -= other.x; y -= other.y;							return *this;															}
+		inline				TCoord2&	operator*=				(double scalar)														noexcept	{ x = (_tBase)(x * scalar); y = (_tBase)(y * scalar);	return *this;															}
+		inline				TCoord2&	operator/=				(double scalar)																	{ x = (_tBase)(x / scalar); y = (_tBase)(y / scalar);	return *this;															}
+		inline				TCoord2&	operator*=				(int64_t scalar)													noexcept	{ x = (_tBase)(x * scalar); y = (_tBase)(y * scalar);	return *this;															}
+		inline				TCoord2&	operator/=				(int64_t scalar)																{ x = (_tBase)(x / scalar); y = (_tBase)(y / scalar);	return *this;															}
+		inline				TCoord2&	operator*=				(uint64_t scalar)													noexcept	{ x = (_tBase)(x * scalar); y = (_tBase)(y * scalar);	return *this;															}
+		inline				TCoord2&	operator/=				(uint64_t scalar)																{ x = (_tBase)(x / scalar); y = (_tBase)(y / scalar);	return *this;															}
 		//
-		inline constexpr	bool		operator==				(const TCoord2& other)										const	noexcept	{ return x == other.x && y == other.y;																					}
-		inline constexpr	bool		operator!=				(const TCoord2& other)										const	noexcept	{ return !operator==(other);																							}
-		inline constexpr	TCoord2		operator-				()															const	noexcept	{ return {x*-1, y*-1};																									}
+		inline constexpr	bool		operator==				(const TCoord2& other)										const	noexcept	{ return x == other.x && y == other.y;																							}
+		inline constexpr	bool		operator!=				(const TCoord2& other)										const	noexcept	{ return !operator==(other);																									}
+		inline constexpr	TCoord2		operator-				()															const	noexcept	{ return {x*-1, y*-1};																											}
 		//
 		template<typename _t>
-		constexpr inline	SCoord2<_t>	Cast					()															const	noexcept	{ return {(_t)x, (_t)y};																								}
-		inline				TCoord2&	Scale					(double scalar)														noexcept	{ return *this *= scalar;																								}
-		inline				TCoord2&	Normalize				()																				{ _tBase sqLen = LengthSquared(); return (sqLen) ? *this /= ::nwol::sqrt_safe(sqLen) : *this;							}
-		inline constexpr	double		Dot						(const TCoord2& other)										const	noexcept	{ return x * other.x + y * other.y;																						}
-		inline constexpr	_tBase		LengthSquared			()															const	noexcept	{ return x * x + y * y;																									}
-		inline constexpr	double		Length					()															const				{ const _tBase sqLen = LengthSquared(); return (sqLen) ? ::sqrt(sqLen) : 0;												}
+		constexpr inline	SCoord2<_t>	Cast					()															const	noexcept	{ return {(_t)x, (_t)y};																										}
+		inline				TCoord2&	Scale					(double scalar)														noexcept	{ return *this *= scalar;																										}
+		inline				TCoord2&	Normalize				()																				{ _tBase sqLen = LengthSquared(); return (sqLen) ? *this /= ::nwol::sqrt_safe(sqLen) : *this;									}
+		inline constexpr	double		Dot						(const TCoord2& other)										const	noexcept	{ return x * other.x + y * other.y;																								}
+		inline constexpr	_tBase		LengthSquared			()															const	noexcept	{ return x * x + y * y;																											}
+		inline constexpr	double		Length					()															const				{ const _tBase sqLen = LengthSquared(); return (sqLen) ? ::sqrt(sqLen) : 0;														}
 		inline				double		AngleWith				(const TCoord2& other)										const				{ const double lengthsProduct = Length() * other.Length(); return lengthsProduct ? ::acos( Dot(other) / lengthsProduct ) : 0;	}
 		inline				void		AddScaled				(const TCoord2& vectorToScaleAndAdd, float scale)								{
 			x								+= vectorToScaleAndAdd.x*scale;
--- nwol/nwol_math.h
+++ nwol/nwol_math.h
@@ -3,41 +3,40 @@
 #include "typeint.h"
 #include <cmath>
 
-#ifndef __NWOL_MATH_H__92836409283642038462309846__
-#define __NWOL_MATH_H__92836409283642038462309846__
+#ifndef NWOL_MATH_H__92836409283642038462309846
+#define NWOL_MATH_H__92836409283642038462309846
 
 namespace nwol
 {
 	// Commonly used constants
-	static											const double		math_pi				= 3.1415926535897932384626433832795;	// Pi
-	static											const double		math_pi2			= 1.5707963267948966192313216916398;	// Pi/2
-	static											const double		math_2pi			= 6.2831853071795864769252867665590;	// 2*Pi
-	static											const double		math_pi_180			= math_pi/180.0;						// Pi/180.0
+	static											const double		math_pi					= 3.1415926535897932384626433832795;	// Pi
+	static											const double		math_pi2				= 1.5707963267948966192313216916398;	// Pi / 2
+	static											const double		math_2pi				= 6.2831853071795864769252867665590;	// 2 * Pi
+	static											const double		math_pi_180				= math_pi/180.0;						// Pi / 180.0
 
-	template<typename _tNumber>	inline constexpr	_tNumber			clamp				(_tNumber _value, _tNumber _min, _tNumber _max)		noexcept	{ return ::nwol::min(_max, ::nwol::max(_min, _value));				}
-	template<typename _tNumber>	inline constexpr	double				sqrt_safe			(_tNumber _valueSquared)							noexcept	{ return _valueSquared ? sqrt(_valueSquared) : 0;					}
-	template<typename _tNumber>	inline constexpr	_tNumber			interpolate_linear	(_tNumber a, _tNumber b, double factor)				noexcept	{ return (_tNumber)(b * factor + a * (1.0 - factor));				}
-	template<typename _tNumber>	inline constexpr	_tNumber			interpolate_cosine	(_tNumber a, _tNumber b, double x)					noexcept	{ return interpolate_linear(a, b, (1.0 - cos(x * math_pi)) * .5);	}
-	template<typename _tNumber>	inline constexpr	_tNumber			mix					(_tNumber a, _tNumber b, double factor)				noexcept	{ return interpolate_linear(a, b, factor);							}
-
+	template<typename _tNumber>	inline constexpr	_tNumber			clamp					(const _tNumber _value, const _tNumber _min, const _tNumber _max)	noexcept	{ return ::nwol::min(_max, ::nwol::max(_min, _value));										}
+	template<typename _tNumber>	inline constexpr	double				sqrt_safe				(const _tNumber _valueSquared)										noexcept	{ return _valueSquared ? sqrt(_valueSquared) : 0;											}
+	template<typename _tNumber>	inline constexpr	_tNumber			interpolate_linear		(const _tNumber a, const _tNumber b, const double factor)			noexcept	{ return (_tNumber)(b * factor + a * (1.0 - factor));										}
+	template<typename _tNumber>	inline constexpr	_tNumber			interpolate_cosine		(const _tNumber a, const _tNumber b, const double x)				noexcept	{ return interpolate_linear(a, b, (1.0 - cos(x * math_pi)) * .5);							}
+	template<typename _tNumber>	inline constexpr	_tNumber			mix						(const _tNumber a, const _tNumber b, const double factor)			noexcept	{ return interpolate_linear(a, b, factor);													}
 	// 
-	static inline									uint64_t			powui				(uint32_t	base, uint32_t	exponent)				noexcept	{ uint64_t	result=1; for( uint32_t i=0; i<exponent; ++i ) result *= base; return result; }
-	static inline									int64_t				powi				(int32_t	base, uint32_t	exponent)				noexcept	{ int64_t	result=1; for( uint32_t i=0; i<exponent; ++i ) result *= base; return result; }
-	static inline									float64_t			powd				(int32_t	base, int32_t	exponent)				noexcept	{ float64_t result=1;
+	static inline									uint64_t			powui					(const uint32_t	base, const uint32_t	exponent)					noexcept	{ uint64_t	result=1; for( uint32_t i=0; i<exponent; ++i ) result *= base; return result;	}
+	static inline									int64_t				powi					(const int32_t	base, const uint32_t	exponent)					noexcept	{ int64_t	result=1; for( uint32_t i=0; i<exponent; ++i ) result *= base; return result;	}
+	static inline									float64_t			powd					(const int32_t	base, const int32_t		exponent)					noexcept	{ float64_t result=1;
 		if( exponent >= 0 )
 			for( int32_t i=0; i<exponent; ++i )
-				result																*= base;
+				result																*= base;	
 		else
 			for( int32_t i=0, count=-exponent; i<count; ++i )
-				result																/= base;
+				result																/= base;	
 		return result;
 	}
 
-	struct	SPairSinCos								{ double Sin, Cos; };	
+	struct	SPairSinCos								{ double Sin, Cos;	};	
 	
-	static inline									SPairSinCos			getSinCos			(double theta)										noexcept	{ return {sin(theta), cos(theta)}; }
-	
+	static inline									SPairSinCos			getSinCos				(const double theta)												noexcept	{ return {sin(theta), cos(theta)};															}
+	static inline constexpr							bool				isPowerOfTwo			(const int num)														noexcept	{ return !(num & (num - 1));																}
 
 } // namespace
 
-#endif // __NWOL_MATH_H__92836409283642038462309846__
+#endif // NWOL_MATH_H__92836409283642038462309846
