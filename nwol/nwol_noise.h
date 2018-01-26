605cf6d2a62c9ce00e6e7f4128b88a5cd8425214
asm128 <pabloarielz@outlook.com>
2018-01-25 22:54:10 -0300

    * Minor formatting fix.

--- nwol/nwol_noise.h
+++ nwol/nwol_noise.h
@@ -8,14 +8,14 @@
 
 namespace nwol
 {
-	double								noise1D				(uint64_t x, uint64_t noiseSeed = 16381)																noexcept;
-	double								noiseNormal1D		(uint64_t x, uint64_t noiseSeed = 16381)																noexcept;
-			double						noise2D				(uint32_t x, uint32_t y				, uint32_t nWidth					, uint64_t noiseSeed = 16381)	noexcept;
-			double						noise3D				(uint32_t x, uint32_t y, uint32_t z	, uint32_t nWidth, uint32_t nHeight	, uint64_t noiseSeed = 16381)	noexcept;
-			double						noiseNormal2D		(uint32_t x, uint32_t y				, uint32_t nWidth					, uint64_t noiseSeed = 16381)	noexcept;
-			double						noiseNormal3D		(uint32_t x, uint32_t y, uint32_t z	, uint32_t nWidth, uint32_t nHeight	, uint64_t noiseSeed = 16381)	noexcept;
+	double		noise1D				(uint64_t x, uint64_t noiseSeed = 16381)																noexcept;
+	double		noiseNormal1D		(uint64_t x, uint64_t noiseSeed = 16381)																noexcept;
+	double		noise2D				(uint32_t x, uint32_t y				, uint32_t nWidth					, uint64_t noiseSeed = 16381)	noexcept;
+	double		noise3D				(uint32_t x, uint32_t y, uint32_t z	, uint32_t nWidth, uint32_t nHeight	, uint64_t noiseSeed = 16381)	noexcept;
+	double		noiseNormal2D		(uint32_t x, uint32_t y				, uint32_t nWidth					, uint64_t noiseSeed = 16381)	noexcept;
+	double		noiseNormal3D		(uint32_t x, uint32_t y, uint32_t z	, uint32_t nWidth, uint32_t nHeight	, uint64_t noiseSeed = 16381)	noexcept;
 	// Based on noiseNormal1D() returns a value between 0 and 1.
-			double						randNoise			(uint64_t seed = 16381)																					noexcept;
+	double		randNoise			(uint64_t seed = 16381)																					noexcept;
 }
 
 #endif // NWOL_NOISE_H__92374092374021784309287349023874
